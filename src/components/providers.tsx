ea556d0d47741acc5a822f03452e2bd19bb843c4
Nutchapon <sr.nutchapon@gmail.com>
2025-12-15 19:46:49 +0700

    04: trpc

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,18 @@
-"use client"
+"use client";
 
 import * as React from "react";
 import dynamic from "next/dynamic";
 
-const ThemeProvider = dynamic(() => import("next-themes").then((mod) => mod.ThemeProvider), {
-  ssr: false,
-});
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
+import { TRPCReactProvider } from "@/trpc/client";
+
+const ThemeProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  {
+    ssr: false,
+  },
+);
 
 export function Providers({
   children,
@@ -13,7 +20,9 @@ export function Providers({
 }: React.ComponentProps<typeof ThemeProvider>) {
   return (
     <ThemeProvider {...props}>
-      {children}
+      <NuqsAdapter>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </NuqsAdapter>
     </ThemeProvider>
   );
 }
